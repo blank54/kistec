94e72b26394418be28b0466cad06f1ca822f8114
Seonghyeon Boris Moon <blank54@snu.ac.kr>
2020-02-03 10:52:48 +0900

    stop tracking running script

--- run_tweet_concat.py
+++
@@ -1,7 +0,0 @@
-# Configuration
-import sys
-sys.path.append("/data/blank54/workspace/kistec/src/")
-from web_crawling import tweet_concat
-
-fdir_concat = "/data/blank54/workspace/kistec_workspace/08_tweeter_crawling/"
-tweet_concat(fdir_concat)
